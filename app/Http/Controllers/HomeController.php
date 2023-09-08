c2909de4a9d73a6ad473876ec22e9629ba29f7f7
DESKTOP-729JV7D\HUATU <xieshaowen@huatu.com>
2023-09-08 15:16:00 +0800

    111

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    // 主页
+    public function index()
+    {
+        return '这是首页';
+    }
+}
