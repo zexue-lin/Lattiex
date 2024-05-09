4b163092e32c31beaeda39a5c3d34ff7262c773e
DESKTOP-729JV7D\HUATU <xieshaowen@huatu.com>
2024-05-09 08:34:22 +0800

    5-9

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        <!-- Fonts测试 -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
